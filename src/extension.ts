b7683f8e008244d6379460c506f110e0aed45cd0
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-12-14 08:15:58 -0500

    clean up, change color

--- src/extension.ts
+++ src/extension.ts
@@ -7,67 +7,67 @@ import { kMaxLength } from 'buffer';
 
 
 export function activate(context: vscode.ExtensionContext) {
-    //let activeEditor = vscode.window.activeTextEditor;
-    let decorationType = vscode.window.createTextEditorDecorationType({
-        backgroundColor: 'rgba(255,255,0,0.3)' // Example color, change as needed
-    });
+	//let activeEditor = vscode.window.activeTextEditor;
+	let decorationType = vscode.window.createTextEditorDecorationType({
+		backgroundColor: 'rgba(255, 136, 0, 0.44)' // Example color, change as needed
+	});
 
-    let run_haskell_terminal = vscode.commands.registerCommand('haskell-interactive.run_haskell_terminal', () => {
-        var editor = vscode.window.activeTextEditor;
+	let run_haskell_terminal = vscode.commands.registerCommand('haskell-interactive.run_haskell_terminal', () => {
+		var editor = vscode.window.activeTextEditor;
 
-        if (!editor) {
-            return;
-        }
-
-        var terminal = getTerminal();
-
-        let rangeToHighlight;
-        if (editor.selection.isEmpty) {
-            var selection = findMultiLines(editor);
-
-            if (selection.max - selection.min === 0) {
-                var text = editor.document.lineAt(editor.selection.start.line).text;
-                rangeToHighlight = new vscode.Range(editor.selection.start.line, 0, editor.selection.start.line, text.length);
-                sendTextToGhci(text, terminal, false);
-            } else {
-                var pos1 = new vscode.Position(selection.min, 0);
-                var pos2 = new vscode.Position(selection.max, editor.document.lineAt(selection.max).text.length);
-                var range = new vscode.Range(pos1, pos2);
-                text = editor.document.getText(range);
-                rangeToHighlight = range;
-                sendTextToGhci(text, terminal, true);
-            }
-        }
-        else {
-            var text = editor.document.getText(editor.selection);
-            if (!text) {
-                return;
-            }
-            rangeToHighlight = new vscode.Range(editor.selection.start, editor.selection.end);
-            sendTextToGhci(text, terminal, true);
-        }
-
-if (editor) {
-	editor.setDecorations(decorationType, [rangeToHighlight]);
-	setTimeout(() => { 
-		if (editor) {
-			editor.setDecorations(decorationType, []); 
+		if (!editor) {
+			return;
 		}
-	}, 3000); // Remove highlight after a delay
+
+		var terminal = getTerminal();
+
+		let rangeToHighlight;
+		if (editor.selection.isEmpty) {
+			var selection = findMultiLines(editor);
+
+			if (selection.max - selection.min === 0) {
+				var text = editor.document.lineAt(editor.selection.start.line).text;
+				rangeToHighlight = new vscode.Range(editor.selection.start.line, 0, editor.selection.start.line, text.length);
+				sendTextToGhci(text, terminal, false);
+			} else {
+				var pos1 = new vscode.Position(selection.min, 0);
+				var pos2 = new vscode.Position(selection.max, editor.document.lineAt(selection.max).text.length);
+				var range = new vscode.Range(pos1, pos2);
+				text = editor.document.getText(range);
+				rangeToHighlight = range;
+				sendTextToGhci(text, terminal, true);
+			}
+		}
+		else {
+			var text = editor.document.getText(editor.selection);
+			if (!text) {
+				return;
+			}
+			rangeToHighlight = new vscode.Range(editor.selection.start, editor.selection.end);
+			sendTextToGhci(text, terminal, true);
+		}
+
+		if (editor) {
+			editor.setDecorations(decorationType, [rangeToHighlight]);
+			setTimeout(() => {
+				if (editor) {
+					editor.setDecorations(decorationType, []);
+				}
+			}, 1000); // Remove highlight after a delay
+		}
+
+		returnFocus();
+	});
+
+
+
+
+	context.subscriptions.push(run_haskell_terminal);
 }
 
-        returnFocus();
-    });
-
-
-	
-
-    context.subscriptions.push(run_haskell_terminal);
-}
-
-function returnFocus(){
+function returnFocus() {
 	let config = vscode.workspace.getConfiguration("haskell-interactive");
-	
+
 	if (config.get("returnFocus") === "editor") {
 		setTimeout(() => {
 			vscode.window.activeTextEditor?.show();
@@ -77,16 +77,16 @@ function returnFocus(){
 
 function getTerminal(name: string = "haskell-interactive") {
 	let config = vscode.workspace.getConfiguration("haskell-interactive");
-	if (config.get("findSharedTerminals")){
+	if (config.get("findSharedTerminals")) {
 		var terminal = vscode.window.terminals.filter((x) => x.name.replace(" [Shared]", "") === name)[0];
-	}else{
+	} else {
 		var terminal = vscode.window.terminals.filter((x) => x.name === name)[0];
 	}
-	
+
 	if (!terminal) {
 		terminal = vscode.window.createTerminal(name);
 		if (name === "haskell-interactive") {
-			let command = config.get("ghciLaunchCommand") as string ;
+			let command = config.get("ghciLaunchCommand") as string;
 			terminal.sendText(command);
 		}
 	}
@@ -139,7 +139,7 @@ function findMultiLines(editor: vscode.TextEditor) {
 
 			min--;
 		}
-		
+
 		while (max < editor.document.lineCount - 1) {
 			let line = editor.document.lineAt(max + 1).text;
 			if (line === "") { break; }
