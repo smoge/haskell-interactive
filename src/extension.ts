177604050f7d6a9e44ace2ab9b4f5f7fa121a01b
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-12-14 07:56:23 -0500

    Added hightlighting to evaluated code

--- src/extension.ts
+++ src/extension.ts
@@ -2,52 +2,67 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import { kMaxLength } from 'buffer';
 
+
+
+
+
 export function activate(context: vscode.ExtensionContext) {
+    //let activeEditor = vscode.window.activeTextEditor;
+    let decorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: 'rgba(255,255,0,0.3)' // Example color, change as needed
+    });
 
+    let run_haskell_terminal = vscode.commands.registerCommand('haskell-interactive.run_haskell_terminal', () => {
+        var editor = vscode.window.activeTextEditor;
 
-	let run_haskell_terminal = vscode.commands.registerCommand('haskell-interactive.run_haskell_terminal', () => {
+        if (!editor) {
+            return;
+        }
 
-		var editor = vscode.window.activeTextEditor;
+        var terminal = getTerminal();
 
-		if (!editor) {
-			return;
+        let rangeToHighlight;
+        if (editor.selection.isEmpty) {
+            var selection = findMultiLines(editor);
+
+            if (selection.max - selection.min === 0) {
+                var text = editor.document.lineAt(editor.selection.start.line).text;
+                rangeToHighlight = new vscode.Range(editor.selection.start.line, 0, editor.selection.start.line, text.length);
+                sendTextToGhci(text, terminal, false);
+            } else {
+                var pos1 = new vscode.Position(selection.min, 0);
+                var pos2 = new vscode.Position(selection.max, editor.document.lineAt(selection.max).text.length);
+                var range = new vscode.Range(pos1, pos2);
+                text = editor.document.getText(range);
+                rangeToHighlight = range;
+                sendTextToGhci(text, terminal, true);
+            }
+        }
+        else {
+            var text = editor.document.getText(editor.selection);
+            if (!text) {
+                return;
+            }
+            rangeToHighlight = new vscode.Range(editor.selection.start, editor.selection.end);
+            sendTextToGhci(text, terminal, true);
+        }
+
+if (editor) {
+	editor.setDecorations(decorationType, [rangeToHighlight]);
+	setTimeout(() => { 
+		if (editor) {
+			editor.setDecorations(decorationType, []); 
 		}
+	}, 3000); // Remove highlight after a delay
+}
 
-		var terminal = getTerminal();
+        returnFocus();
+    });
 
-		if (editor.selection.isEmpty) {
 
-			let parts = editor.document.lineAt(editor.selection.start.line).text.split(" ");
-			let excludedFunctions = vscode.workspace.getConfiguration("haskell-interactive").get("excludedFunctionNames") as Array<string>;
+	
 
-			var selection = findMultiLines(editor);
-
-			if (selection.max - selection.min === 0 || excludedFunctions.indexOf(parts[0]) >= 0) {
-				var text = editor.document.lineAt(editor.selection.start.line).text;
-				sendTextToGhci(text, terminal, false);
-			} else {
-
-				var pos1 = new vscode.Position(selection.min, 0);
-				var pos2 = new vscode.Position(selection.max, editor.document.lineAt(selection.max).text.length);
-				var range = new vscode.Range(pos1, pos2);
-				text = editor.document.getText(range);
-				sendTextToGhci(text, terminal, true);
-
-			}
-		}
-		else {
-			var text = editor.document.getText(editor.selection);
-			if (!text) {
-				return;
-			}
-
-			sendTextToGhci(text, terminal, true);
-		}
-
-		returnFocus();
-
-	});
-	context.subscriptions.push(run_haskell_terminal);
+    context.subscriptions.push(run_haskell_terminal);
 }
 
 function returnFocus(){
